1e9f0b343415053512bb6fac341783b9b93d06bf
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-01-26 18:24:04 +0530

    code updated

--- frontend/src/components/Logo/Logo.tsx
+++
@@ -1,5 +0,0 @@
-import './Logo.scss';
-
-interface LogoProps { }
-
-export const Logo = (props: LogoProps) => <h1 className="logo">MessageNode</h1>;
--- frontend/src/pages/Auth/Auth.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-import './Auth.scss';
-
-interface AuthProps {
-    children: ReactNode;
-}
-
-export const Auth = (props: AuthProps) => <section className="auth-form">{props.children}</section>;
