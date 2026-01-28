4cd7b683c789ca96d89258f7a07fc6a81ed4ebad
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-01-28 16:59:51 +0530

    code updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -97,7 +97,7 @@ app.use(
     schema: graphqlSchema,
     rootValue: resolvers,
     graphiql: true,
-    formatError(err) {
+    customFormatErrorFn(err) {
       if (!err.originalError) {
         return err;
       }
